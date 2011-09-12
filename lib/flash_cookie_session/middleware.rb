82a72c51ec436170937b026f886a8ce7d43b12e9
Selem Delul <selem@selem.im>
2011-09-12 12:59:09 -0400

    Force middleware usage with a form parameter.

--- lib/flash_cookie_session/middleware.rb
+++ lib/flash_cookie_session/middleware.rb
@@ -6,15 +6,16 @@ module FlashCookieSession
     end
 
     def call(env)
-      if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
-        req = Rack::Request.new(env)
+      req = Rack::Request.new(env)
+
+      if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/ || req.params['force_uploader'] == "true"
         the_session_key = [ @session_key, req.params[@session_key] ].join('=').freeze if req.params[@session_key]
         the_remember_token = [ 'remember_token', req.params['remember_token'] ].join('=').freeze if req.params['remember_token']
         cookie_with_remember_token_and_session_key = [ the_remember_token, the_session_key ].join(';').freeze
         env['HTTP_COOKIE'] = cookie_with_remember_token_and_session_key
         env['HTTP_ACCEPT'] = "#{req.params['_http_accept']}".freeze if req.params['_http_accept']
       end
-      
+
       @app.call(env)
     end
   end
